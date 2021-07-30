f6428b47773f111249297a150b413f69ded4ccf8
maissarek <61662097+maissarek@users.noreply.github.com>
2021-07-30 20:00:38 +0100

    ✓

--- app/Models/Enseigante.php
+++ app/Models/Enseigante.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Enseigante extends Model
 {
     use HasFactory;
-   protected $table="Enseigante";
+   protected $table="enseigante";
 protected $fillable= ['id','experienceTeaching','lieuKhatm','dateKhatm','ensKhatm','Remplace','Rempl_day']
 }
