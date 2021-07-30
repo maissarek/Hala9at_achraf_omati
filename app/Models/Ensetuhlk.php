a22fe5eab0b773b5aa75639e40c1ce4f366bd5fa
maissarek <61662097+maissarek@users.noreply.github.com>
2021-07-30 19:55:46 +0100

    À

--- app/Models/Ensetuhlk.php
+++ app/Models/Ensetuhlk.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Ensetuhlk extends Model
 {
     use HasFactory;
+protected $table="ensetuhlk";
+protected $fillable= ['id','personne_id','username','password','dateCreation','etat']
 }
