e0e955600223a58f4f48caef661c29d15c8d4761
maissarek <61662097+maissarek@users.noreply.github.com>
2021-07-31 11:41:36 +0100

    ✓

--- app/Models/Histhalaka.php
+++ app/Models/Histhalaka.php
@@ -8,12 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Histhalaka extends Model
 {
     use HasFactory;
-$table->integer('id')->primary();
-            $table->integer('ensRemplacante')->index('ensRemplacante');
-            $table->date('date');
-            $table->string('moraja3a', 100);
-            $table->string('moton', 500);
-            $table->string('autres', 5000);
-            $table->string('absence_Ens', 10);
-            $table->string('justification_Ens',
+
+protected $table="histhalaka";
+protected $fillable=['id','ensRemplacante','date','moraja3a','moton','autres','absence_Ens','justification_Ens']
+
 }
