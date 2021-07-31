9bf6098150dc877d7b3d492f2d291815615bb03b
maissarek <61662097+maissarek@users.noreply.github.com>
2021-07-31 11:46:09 +0100

    ✓

--- app/Models/Personne.php
+++ app/Models/Personne.php
@@ -8,14 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class personne extends Model
 {
     use HasFactory;
-$table->integer('id')->primary();
-            $table->string('nom', 100);
-            $table->string('prenom', 100);
-            $table->date('dateNaiss');
-            $table->string('adresse', 1000);
-            $table->integer('telephone');
-            $table->string('email', 50);
-            $table->string('fonction', 500);
-            $table->string('niveauScolaire', 50);
-            $table->date('dateEntree');
+
+protected $table="personne";
+protected $fillable=['id','nom','prenom','dateNaiss','adresse','telephone','email', 'fonction', 'niveauScolaire','dateEntree']
+
 }
