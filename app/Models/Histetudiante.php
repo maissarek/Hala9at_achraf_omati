f3330a3a9fe589a611bd32f679393a797ae0784d
maissarek <61662097+maissarek@users.noreply.github.com>
2021-07-31 11:36:30 +0100

    ✓

--- app/Models/Histetudiante.php
+++ app/Models/Histetudiante.php
@@ -8,17 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Histetudiante extends Model
 {
     use HasFactory;
-$table->integer('id')->primary();
-            $table->integer('HistHalaka')->index('HistHalaka');
-            $table->integer('ensEtudHlk_id')->index('ensEtudHlk_id');
-            $table->integer('hizb');
-            $table->string('surat', 50);
-            $table->integer('aya_d');
-            $table->integer('aya_f');
-            $table->integer('mtn_name');
-            $table->integer('mtn_num_d');
-            $table->integer('mtn_num_f');
-            $table->tinyInteger('absent');
-            $table->string('justificatif', 500);
-            $table->string('observations', 
+ protected $table =;
+protected $fillable=['id','HistHalaka','ensEtudHlk_id','hizb','surat','aya_d','aya_f','mtn_name','mtn_num_d','mtn_num_f','absent','justificatif','observations'] 
 }
