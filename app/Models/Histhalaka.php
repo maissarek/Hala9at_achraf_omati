da3e8293a41956bdaf9ac7a05a2cce74667748bc
maissarek <61662097+maissarek@users.noreply.github.com>
2021-07-31 11:27:57 +0100

    Update Histhalaka.php

--- app/Models/Histhalaka.php
+++ app/Models/Histhalaka.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Histhalaka extends Model
 {
     use HasFactory;
+$table->integer('id')->primary();
+            $table->integer('ensRemplacante')->index('ensRemplacante');
+            $table->date('date');
+            $table->string('moraja3a', 100);
+            $table->string('moton', 500);
+            $table->string('autres', 5000);
+            $table->string('absence_Ens', 10);
+            $table->string('justification_Ens',
 }
