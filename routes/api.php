bf32109416fb6fc90cefc2d180f8785a109588aa
maissarek <61662097+maissarek@users.noreply.github.com>
2021-08-01 10:54:14 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('',[,]);
+Route::post('',[,]);
+Route::post('',[,]);
+Route::put('',[,]);
+Route::delete('',[,]);
