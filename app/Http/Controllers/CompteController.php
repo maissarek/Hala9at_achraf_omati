4a7dc72ec1e2e4f8f9d4ca7a9ce8ae8a51667688
maissarek <61662097+maissarek@users.noreply.github.com>
2021-08-03 12:02:10 +0100

    Update CompteController.php

--- app/Http/Controllers/CompteController.php
+++ app/Http/Controllers/CompteController.php
@@ -17,6 +17,45 @@ class CompteController extends Controller
 return response()->json(Compte::all(),200);
         }
 
+public function store(Request $request)
+    {
+        $compte=  Compte::create($request->all());
+        return response($compte,201);
+    }
+
+public function show($id)
+    {
+        $compte=Compte::find($id);
+        if(is_null($compte)){
+
+           return response()->json(['message'=>'Compte not found',404]);
+}
+           return response()->json($compte::find($id),200);
+    }
+
+public function update(Request $request,$id)
+    {
+        $compte= Compte::find($id);
+        if(is_null($compte)){
+
+           return response()->json(['message'=>'Compte not found',404]);
+}
+$compte->update ($request->all());
+return response($compte,201);
+    }
+
+public function destroy(Compte $compte)
+    {
+        $compte= Compte::find($id);
+        if(is_null($compte)){
+
+           return response()->json(['message'=>'Compte not found',404]);
+}
+$compte->delete();
+return response()->json(null,204);
+    }
+
+
     /**
      * Show the form for creating a new resource.
      *
@@ -33,28 +72,14 @@ return response()->json(Compte::all(),200);
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        $compte=  Compte::create($request->all());
-        return response($compte,201);
-    }
-
+    
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Compte  $compte
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        $compte=Compte::find($id);
-        if(is_null($compte)){
-
-           return response()->json(['message'=>'Compte not found',404]);
-}
-           return response()->json($compte::find($id),200);
-    }
-
+    
     /**
      * Show the form for editing the specified resource.
      *
@@ -73,31 +98,12 @@ return response()->json(Compte::all(),200);
      * @param  \App\Models\Compte  $compte
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request,$id)
-    {
-        $compte= Compte::find($id);
-        if(is_null($compte)){
-
-           return response()->json(['message'=>'Compte not found',404]);
-}
-$compte->update ($request->all());
-return response($compte,201);
-    }
-
+    
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Compte  $compte
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Compte $compte)
-    {
-        $compte= Compte::find($id);
-        if(is_null($compte)){
-
-           return response()->json(['message'=>'Compte not found',404]);
-}
-$compte->delete();
-return response()->json(null,204);
-    }
+    
 }
