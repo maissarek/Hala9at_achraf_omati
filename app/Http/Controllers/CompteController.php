bcc142c6e2f5f2785e970fc2590a16defdfdab40
maissarek <61662097+maissarek@users.noreply.github.com>
2021-08-03 16:33:13 +0100

    ✓

--- app/Http/Controllers/CompteController.php
+++ app/Http/Controllers/CompteController.php
@@ -11,7 +11,7 @@ class CompteController extends Controller
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
-     */
+     */ 
     public function index()
     {
 return response()->json(Compte::all(),200);
